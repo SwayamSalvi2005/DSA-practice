4504e3c0947228dc8c6ec9839349e5cb6ef03d0e
SwayamSalvi2005 <swayamnsalvi@gmail.com>
2026-08-06 14:02:24 +0530

    leetcode 268

---
+++ src/arrays/MissingNumber_268.java
@@ -0,0 +1,24 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class MissingNumber_268 {
+    public int missingNumber(int[] nums) {
+
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        if(nums[0] != 0){
+            return 0;
+        }
+
+        for(int i =0; i<n-1; i++){
+            if(nums[i]+1 != nums[i+1]){
+                return nums[i]+1;
+            }
+        }
+
+        return n;
+
+    }
+}
